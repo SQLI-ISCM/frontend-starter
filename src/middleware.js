3809632092f82d4086c539ad1aaf6851084d9481
anas jaidi <anas.jaidi@icloud.com>
2024-06-13 14:14:53 +0100

    fix: middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,2 +1,8 @@
 
-export {config, middleware} from '@sqli/core/ui'
+export { middleware } from '@sqli/core/middleware';
+
+export const config = {
+	matcher: [
+		"/((?!api|_next/static|_next/image|favicon|assets).*)"
+	],
+};
